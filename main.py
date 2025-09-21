df7c594a73010db6a525b503a8ea06667f1d7585
k-ing-ing <1762507696@qq.com>
2025-09-21 16:56:14 +0800

    完成“回文数判断”作业

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-user_input = input("请输入一个5位数字：")
-if not user_input.isdigit():
-     print("错误提示：输入不是纯数字")
-elif len(user_input) != 5:
-     print("错误提示：输入不是5位数字")
+num = input("请输入一个5位数字：")
+if not (num.isdigit() and len(num) == 5):
+    print("输入错误: 请输入5位数字")
 else:
-
-     if user_input == user_input[::-1]:
-         print("是回文数")
-     else:
-         print("不是回文数")
+    if num == num[::-1]:
+        print(f"{num} -> 是回文数")
+    else:
+        print(f"{num} -> 不是回文数")
